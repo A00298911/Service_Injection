5eef618cda7ae86e582f9714e6f106a1a8329cd7
A00298911 <162391972+A00298911@users.noreply.github.com>
2024-10-04 22:38:41 +0000

    Implementing CalculatorService.

---
+++ Interfaces/ICalculatorService.cs
@@ -0,0 +1,7 @@
+public interface ICalculatorService<T>
+{
+    T Add(T a, T b);
+    T Subtract(T a, T b);
+    T Multiply(T a, T b);
+    T Divide(T a, T b);
+}
---
+++ Services/CalculatorService.cs
@@ -0,0 +1,7 @@
+public class CalculatorService : ICalculatorService<int>
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+    public int Multiply(int a, int b) => a * b;
+    public int Divide(int a, int b) => b != 0 ? a / b : throw new DivideByZeroException();
+}
